6bcaf717bd487ca848dd2544700becd8a7c59baa
Jiayi Jiang <92309998+jyjsunny@users.noreply.github.com>
2024-10-10 18:29:58 +0800

    merge_branch_add_scriptsFolder

---
+++ Artifact_Evaluation/BinPRE_scripts/run_modbus_client.sh
@@ -0,0 +1,7 @@
+
+
+cd ../../Analyzer
+
+python3 fsend_split.py modbus 0 0 oa xx big 0
+#wait and enter threadId: 1
+#Total Analyze Time:416.99148750305176
---
+++ Artifact_Evaluation/BinPRE_scripts/run_modbus_server.sh
@@ -0,0 +1,7 @@
+
+
+
+cd ../../src
+
+echo '  [!!!!!!!!!!!!!!!!!!!!]Users (reviewers), please note that a letter 'e' needs to be entered here in order to switch modes.'
+./run run taint ./freemodbus/tcpmodbus
